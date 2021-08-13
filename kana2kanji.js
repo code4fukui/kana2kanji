b38038c69310951b79d4659fa3cd3ffcd0d57223
Taisuke Fukuno <fukuno@jig.jp>
2021-08-13 10:59:14 +0900

    remove debug out

--- kana2kanji.js
+++ kana2kanji.js
@@ -2,7 +2,6 @@ const endpoint = "http://www.google.com/transliterate";
 
 const kana2kanji = async (s) => {
   const res = await (await fetch(endpoint + "?langpair=ja-Hira|ja&text=" + encodeURIComponent(s))).json();
-  console.log(res);
   return res;
 };
 
